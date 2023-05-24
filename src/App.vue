c0f3de87877ff58b7ced4d34b9242111674cc9df
M_Shrief <m.shrief.dev@proton.me>
2023-05-24 10:31:38 +0300

    removed layout, added navbar and footer and component transition, also added mixin.scss to css file

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,42 @@
 <template>
-  <router-view />
+  <header>
+    <Navbar />
+  </header>
+  <router-view v-slot="{ Component }">
+    <transition name="fade" appear>
+      <component :is="Component" />
+    </transition>
+  </router-view>
+  <footer>
+    <p>
+      Copyright &copy; 2022 |
+      <a href="#" title="Github profile" class="profile">M. Shrief</a>
+    </p>
+  </footer>
 </template>
 
 <script setup lang="ts">
-
+import Navbar from './components/Navbar.vue';
 </script>
+
+<style lang="scss">
+@import './css/mixins.scss';
+@include fade-transition;
+
+footer {
+  text-align: center;
+  margin-top: 1.6rem;
+  font-size: 1.2rem;
+  font-weight: 700;
+
+  .profile {
+    color: #004e64;
+    text-decoration: none;
+  }
+
+  @include mQ($breakpoint-sm) {
+    margin-top: 1rem;
+    font-size: 1rem;
+  }
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,112 @@
+<template>
+  <!-- Navigation Raws -->
+  <nav id="nav-raw" dir="rtl">
+    <router-link to="/" id="logo">أديب</router-link>
+    <div class="nav-row-group">
+      <router-link to="/ordering" class="nav-item"
+        active-class="active">طلباتك</router-link>
+      <router-link to="/history" class="nav-item" active-class="active">مراجعة
+        الطلبات</router-link>
+      <router-link to="/about" class="nav-item"
+        active-class="active">قصتنا</router-link>
+
+      <router-link to="/partners" class="nav-item" active-class="active">كن
+        شريكاً</router-link>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts" setup>
+import { useRouter } from 'vue-router';
+import { computed } from '@vue/reactivity';
+// stores
+// import { useOrderStore } from '@/stores/orders';
+// import { usePartnerStore } from '@/stores/partners';
+
+// const router = useRouter();
+
+// const parnterStore = usePartnerStore();
+// const partner = computed(() => {
+//   return parnterStore.getPartner
+// })
+
+// const orderStore = useOrderStore();
+// async function logout() {
+//   parnterStore.logout()
+//   orderStore.reset()
+//   router.push('/');
+// }
+
+</script>
+
+<style lang="scss" scoped>
+@import '../css/mixins.scss';
+
+#nav-raw {
+  display: flex;
+  flex-direction: row;
+  background-color: #1f2124;
+  justify-content: space-between;
+  align-items: center;
+
+  #logo {
+    color: rgba($color: #f6b352, $alpha: .8);
+    text-decoration: none;
+    font-size: 1.4rem;
+    font-weight: 700;
+    padding: 0.2rem 0.4rem;
+    margin: 0.4rem;
+
+    &:hover {
+      color: #f6b352;
+    }
+
+    @include mQ($breakpoint-sm) {
+      font-size: 1rem;
+      padding: 0.1rem 0.2rem;
+      margin: 0.1rem;
+    }
+  }
+
+  $mainColor: #FBE6C2;
+
+  .nav-item {
+    padding: 0.2rem 0.4rem;
+    margin: 0.4rem;
+    color: rgba($color: $mainColor, $alpha: 0.8);
+    font-size: 1.2rem;
+    text-decoration: none;
+
+    &:hover {
+      color: $mainColor;
+    }
+
+    @include mQ($breakpoint-sm) {
+      font-size: 0.8rem;
+      padding: 0.1rem 0.2rem;
+      margin: 0.1rem;
+    }
+  }
+
+  .nav-item.active {
+    background-color: $mainColor;
+    color: #2c3e50;
+    border-radius: 1rem;
+  }
+
+  .partner-name {
+    background-color: $mainColor;
+    color: #2c3e50;
+    border-radius: 1rem;
+    padding: 0.2rem 0.4rem;
+    margin: 0.4rem;
+    font-size: 1rem;
+
+    @include mQ($breakpoint-sm) {
+      font-size: 0.8rem;
+      padding: 0.1rem 0.2rem;
+      margin: 0.1rem;
+    }
+  }
+}
+</style>
