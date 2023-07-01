f8bd93873274797670c245349dda29000e4f7d01
M-Shrief <m.shrief.dev@proton.me>
2023-07-01 12:37:24 +0300

    added component testing for PreviewPrints

--- src/components/PreviewColors.cy.ts
+++ src/components/PreviewColors.cy.ts
@@ -1,8 +1,18 @@
-import PreviewColors from './components/PreviewColors.vue'
+import PreviewColors from './PreviewColors.vue';
 
 describe('<PreviewColors />', () => {
   it('renders', () => {
     // see: https://on.cypress.io/mounting-vue
-    cy.mount(PreviewColors)
-  })
-})
+    const onColorSpy = cy.spy().as('onColorSpy');
+    cy.mount(PreviewColors, {
+      props: {
+        onColor: onColorSpy,
+        colors: ['#000'],
+      },
+    });
+
+    cy.get('div>.color').click();
+    cy.get('@onColorSpy').should('have.been.calledWith', '#000');
+    cy.get('@onColorSpy').should('be.calledOnce');
+  });
+});
